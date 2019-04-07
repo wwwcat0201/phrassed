e0309c4d548fb33fbb81fb610e55ca326c4744b0
Koen van Gilst <koen.vangilst@gmail.com>
2019-04-07 12:20:33 +0200

    chore(tests) - better tests

---
+++ integration/home.test.js
@@ -0,0 +1,40 @@
+const supertest = require("supertest")
+
+const request = supertest("http://localhost:3000")
+
+describe("When loading the home page", () => {
+  it("should respond with 200 ok", () => {
+    return request.get("/").expect(200)
+  })
+
+  it("should have title containing name of app", () => {
+    return request.get("/").then(response => {
+      document.documentElement.innerHTML = response.text
+      expect(document.title).toContain("Phrassed")
+    })
+  })
+})
+
+describe("When sending a query", () => {
+  it("should contain the translations", async () => {
+    const response = await request.get("/?q=Anlage")
+    document.documentElement.innerHTML = response.text
+    expect(response.text).toContain("IATE-1104363")
+    expect(response.text).toContain("Anlage")
+    expect(response.text).toContain("belegging")
+    expect(response.text).toContain("investment")
+  })
+  it("should contain example sentence", async () => {
+    const response = await request.get("/?q=Anlage")
+    document.documentElement.innerHTML = response.text
+    expect(response.text).toContain(
+      "Ich kann dir nicht sagen ob das eine gute Anlage ist."
+    )
+    expect(response.text).toContain(
+      "Ik kan je niet zeggen of dat een goede investering is."
+    )
+    expect(response.text).toContain(
+      "I can&#39;t tell you if that&#39;s a good investment."
+    )
+  })
+})
---
+++ server/handlers/__tests__/handlers.test.js
@@ -0,0 +1,7 @@
+// const handlers = require("../")
+
+describe("#renderRoot", () => {
+  it("should render without crashing", () => {
+    expect(true).toBe(true)
+  })
+})
